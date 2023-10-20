fe287532604a899649a5ff4fab3573f287291d53
diogoDSN <diogofgsnogueira@gmail.com>
2023-10-20 20:50:17 +0200

    Changed canvas and canvas_image from local variables to class properties

--- EX1/ExampleGUI/gui.py
+++ EX1/ExampleGUI/gui.py
@@ -17,13 +17,13 @@ class MainGUI():
     def restart_scenario(self, ):
         print('restart not implemented yet')
     
-    def load_simulation(self, path, canvas, canvas_image):
+    def load_simulation(self, path):
         self.sc =  Scenario(0, 0, file_path=path)
-        self.sc.to_image(canvas, canvas_image)
+        self.sc.to_image(self.canvas, self.canvas_image)
         
 
 
-    def step_scenario(self, canvas, canvas_image):
+    def step_scenario(self):
         """
         Moves the simulation forward by one step, and visualizes the result.
 
@@ -33,7 +33,7 @@ class MainGUI():
             canvas_image (missing _type_): Add _description_
         """
         self.sc.update_step()
-        self.sc.to_image(canvas, canvas_image)
+        self.sc.to_image(self.canvas, self.canvas_image)
 
 
     def exit_gui(self, ):
@@ -61,9 +61,9 @@ class MainGUI():
         file_menu.add_command(label='Restart', command=self.restart_scenario)
         file_menu.add_command(label='Close', command=self.exit_gui)
 
-        canvas = Canvas(win, width=Scenario.GRID_SIZE[0], height=Scenario.GRID_SIZE[1])  # creating the canvas
-        canvas_image = canvas.create_image(5, 50, image=None, anchor=tkinter.NW)
-        canvas.pack()
+        self.canvas = Canvas(win, width=Scenario.GRID_SIZE[0], height=Scenario.GRID_SIZE[1])  # creating the canvas
+        self.canvas_image = self.canvas.create_image(5, 50, image=None, anchor=tkinter.NW)
+        self.canvas.pack()
 
         self.sc = Scenario(100, 100)
 
@@ -79,18 +79,18 @@ class MainGUI():
         ]
 
         # can be used to show pedestrians and targets
-        self.sc.to_image(canvas, canvas_image)
+        self.sc.to_image(self.canvas, self.canvas_image)
 
         # can be used to show the target grid instead
         # sc.target_grid_to_image(canvas, canvas_image)
 
-        btn = Button(win, text='Step simulation', command=lambda: self.step_scenario(canvas, canvas_image))
+        btn = Button(win, text='Step simulation', command=lambda: self.step_scenario())
         btn.place(x=20, y=10)
         btn = Button(win, text='Restart simulation', command=self.restart_scenario)
         btn.place(x=160, y=10)
         btn = Button(win, text='Create simulation', command=self.create_scenario)
         btn.place(x=315, y=10)
-        btn = Button(win, text='Load simulation', command=lambda: self.load_simulation('scenarios/test_scenario.json', canvas, canvas_image))
+        btn = Button(win, text='Load simulation', command=lambda: self.load_simulation('scenarios/test_scenario.json'))
         btn.place(x=470, y=10)
         
 
