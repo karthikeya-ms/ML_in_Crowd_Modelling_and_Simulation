1037ab69e047133df4663052cc42aaecca86e95e
diogoDSN <diogofgsnogueira@gmail.com>
2023-10-25 17:38:43 +0200

    Added heatmap visualization to the scenario gui

--- EX1/ExampleGUI/scenario_gui.py
+++ EX1/ExampleGUI/scenario_gui.py
@@ -97,6 +97,8 @@ class ScenarioGUI:
         The scenario to be rendered. This attribute should not be directly manipulated.
     grid_mode : bool
         If set to False the elements will be rendered on a uniform background (no visible cells).
+    heatmap_mode : bool
+        If set to True the cells will be rendered in heatmap mode (takes priority over grid_mode).
     canvas : tkinter.Canvas
         The canvas used to draw the scenario.
     scenario : tkinter.Scenario
@@ -125,21 +127,24 @@ class ScenarioGUI:
     MIN_OBSTACLE_SIDE = 3
 
     ELEMENT_TAG = "element"
+    HEATMAP_TAG = "heatmap"
     
     OBSTACLE_HOLD_SECONDS = 0.5
 
-    def __init__(self, master: tk.Frame, scen: Scenario, grid_mode: bool =False):
+    def __init__(self, master: tk.Frame, scen: Scenario, grid_mode: bool =False, heatmap_mode: bool =False):
         """Creates an instance of the ScenarioGUI class. Also creates the canvas for the simulation and draws it.
 
         Args:
             master (tkinter.Frame): The tkinter frame object in which the grid will be rendered.
             scen (Scenario): The initial scenario to be rendered in the grid.
             grid_mode (bool, optional): Initial value of the grid_mode property. Defaults to False.
+            heatmap_mode (bool, optional): Initial value of the heatmap_mode property. Defaults to False.
         """
         self.master = master
 
         self._scenario = scen
         self.grid_mode = grid_mode
+        self.heatmap_mode = heatmap_mode
 
         self.left_mouse_button = LeftMouseButton(self, ScenarioGUI.OBSTACLE_HOLD_SECONDS)
         self.pedestrian_pos = None
@@ -158,7 +163,7 @@ class ScenarioGUI:
             self.canvas.bind("<Button-3>", self.on_right_click)
 
         #drawing the initial grid
-        self.draw_grid()
+        self.draw_scenario()
 
     @property
     def scenario(self) -> Scenario:
@@ -179,7 +184,7 @@ class ScenarioGUI:
             This setter ensures these procedures are executed when the scenario is updated.
         """
         self._scenario = scen
-        self.draw_grid()
+        self.draw_scenario()
 
     @property
     def grid_side(self) -> int:
@@ -232,7 +237,7 @@ class ScenarioGUI:
         """
         return max(ScenarioGUI.MIN_OBSTACLE_SIDE, self.cell_side)
 
-    def draw_grid(self):
+    def draw_scenario(self):
         """
             Draws the grid from an empty canvas. This method should only be invoked when the grid itself changes, 
             for example, when the number of cells is updated or the scenario itself is changed. 
@@ -241,18 +246,53 @@ class ScenarioGUI:
 
         self.canvas.create_rectangle(0, 0, self.canvas_side, self.canvas_side, fill=ScenarioGUI.BACKGROUND_COLOR)
 
-        if self.grid_mode:
-            for i in range(1, self.grid_side):
-                x = i*self.cell_side
-                self.canvas.create_line(x, 0, x, self.canvas_side, fill=ScenarioGUI.SEPARATOR_COLOR)
+        if self.heatmap_mode:
+            self.draw_target_heatmap()
+        elif self.grid_mode:
+            self.draw_grid()
+
+        self.update_scenario()
+    
+    def draw_grid(self):
+        """Draws the grid.
+        """
+        for i in range(1, self.grid_side):
+            x = i*self.cell_side
+            self.canvas.create_line(x, 0, x, self.canvas_side, fill=ScenarioGUI.SEPARATOR_COLOR)
             
-            for i in range(1, self.grid_side):
-                y = i*self.cell_side
-                self.canvas.create_line(0, y, self.canvas_side, y, fill=ScenarioGUI.SEPARATOR_COLOR)
+        for i in range(1, self.grid_side):
+            y = i*self.cell_side
+            self.canvas.create_line(0, y, self.canvas_side, y, fill=ScenarioGUI.SEPARATOR_COLOR)
 
-        self.update_grid()
+    def draw_target_heatmap(self):
+        """
+            Draws the target heatmap on the canvas. 
+            This heatmap visualizes the distance from each cell to the nearest target.
+        """
+        def get_hex_number(target_distance: float, color_term: str):
+            h = hex(max(0, min(255, int(10 * target_distance) - color_term * 255)))[2:]
+            
+            if len(h) == 1:
+                h = '0' + h
 
-    def update_grid(self):
+            return h
+        
+        for x in range(self.grid_side):
+            for y in range(self.grid_side):
+                target_distance = self.scenario.target_distance_grids[x][y]
+                r = get_hex_number(target_distance, 0)
+                g = get_hex_number(target_distance, 1)
+                b = get_hex_number(target_distance, 2)
+                self.canvas.create_rectangle(
+                    x*self.cell_side,
+                    y*self.cell_side,
+                    (x+1)*self.cell_side,
+                    (y+1)*self.cell_side,
+                    fill=f'#{r}{g}{b}',
+                    tag=ScenarioGUI.HEATMAP_TAG
+                )
+
+    def update_scenario(self):
         """
             All elements on the grid are redrawn in their updated positions. 
             This methods does not perform an update step in the scenario, 
@@ -337,8 +377,10 @@ class ScenarioGUI:
         ):
             self.scenario.targets.add(pos)
             self.scenario.target_distance_grids = self.scenario.recompute_target_distances()
-            self.update_grid()
-
+            if self.heatmap_mode:
+                self.draw_scenario()
+            else:
+                self.update_scenario()
 
     def on_left_press(self, event: tk.Event):
         """
@@ -384,8 +426,7 @@ class ScenarioGUI:
             self.scenario.pedestrians.append(Pedestrian(pos,1))
 
         self.pedestrian_pos = None
-        self.update_grid()
-
+        self.update_scenario()
 
     def on_hold_or_drag(self, event: tk.Event):
         """
@@ -398,7 +439,7 @@ class ScenarioGUI:
         obstacle = (int(event.x // self.cell_side), int(event.y // self.cell_side))
         if not (obstacle in self.scenario.targets or obstacle in self.pedestrian_pos):
             self.scenario.obstacles.add(obstacle)
-            self.update_grid()
+            self.update_scenario()
 
 
 if __name__ == "__main__":
