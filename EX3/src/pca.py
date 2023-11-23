1c3423a73b3344fe94e8ca4bf84f173fec9021fd
Mohammed El-Shaikh <mel.shaikh@tum.de>
2023-11-23 02:34:35 +0100

    Code cleanup

--- EX3/src/pca.py
+++ EX3/src/pca.py
@@ -1,33 +1,38 @@
 # This module tells Python to ignore annotations; they are only for the reader
+# Required to annotate the return type of the static method `pca`
 from __future__ import annotations
 
 import numpy as np
-import matplotlib.pyplot as plt
+
+# Type alias
+ndarray = np.ndarray
 
 
 class PCA:
-    U: np.ndarray
-    S: np.ndarray
-    Vh: np.ndarray
-    _mean = None
-    _transpose: bool = False
+    # U, S, Vh as obtained from singular value decomposition such that data_matrix == U @ diag(S) @ Vh
+    U: ndarray
+    S: ndarray
+    Vh: ndarray
 
-    @property
-    def mean(self) -> np.ndarray:
-        if self._mean is None:
-            # throw error
-            raise ValueError("PCA not performed")
-        return self._mean
+    # The average data point, typically the average row. If the data was transposed before performing PCA,
+    # then the average data point is the average column of the data mabtrix
+    mean: ndarray
+
+    # Whether the data was transposed before performing PCA
+    # This flag is set by the caller of the `pca` static method
+    # The flag is used in `reverse_pca` to determine whether or to transpose the reconstructed data
+    _transpose: bool = False
 
     @property
     def energy(self):
         return self.S ** 2 / np.sum(self.S ** 2)
 
-    def __init__(self, u, s, vh):
+    def __init__(self, u, s, vh: ndarray, mean: ndarray, transpose: bool = False):
         self.U = u
         self.S = s
         self.Vh = vh
-        self._mean = None
+        self.mean = mean
+        self._transpose = transpose
         # User can input anything; make sure that the dimensions are valid
         self.validate()
 
@@ -35,8 +40,12 @@ class PCA:
         yield self.U
         yield self.S
         yield self.Vh
+        yield self.mean
 
-    def validate(self):
+    def validate(self) -> None:
+        """
+        Paranoid checks to make sure that the dimensions are valid
+        """
         num_singular_values = self.S.shape[0]
         expected_num_singular_values = min(self.U.shape[0], self.Vh.shape[0])
         # U and Vh are 2D matrices; S is the 1D vector of singular values (including multiplicities)
@@ -44,30 +53,34 @@ class PCA:
         # The matrix product U @ diag(s) @ Vh must be defined, hence check the dimensions
         assert self.U.shape[0] == self.U.shape[1] and self.Vh.shape[0] == self.Vh.shape[1]
         assert num_singular_values == expected_num_singular_values
-        # If we did everything correctly, the energy should be 1. Check that it is close to 1
+        # If we did everything correctly, the sum of all energies should be 1. Check that it is close to 1
         assert np.abs(sum(self.energy) - 1) < 1e-10
         # Check that U and Vh are orthogonal/unitary
         assert np.allclose(self.U @ self.U.T, np.eye(self.U.shape[0]))
         assert np.allclose(self.Vh @ self.Vh.T, np.eye(self.Vh.shape[0]))
-        if self._mean is not None:
-            assert isinstance(self._mean, np.ndarray)
-            assert len(self._mean.shape) == 1
-            assert self._mean.shape[0] == self.Vh.shape[0]
+        # Check that the mean is a 1D vector
+        assert len(self.mean.shape) == 1
+        # Check that the mean has the same dimension as a data point
+        assert self.mean.shape[0] == self.Vh.shape[0]
 
-    def reverse_pca(self, r: int = -1, add_mean: bool = True) -> np.ndarray:
+    def reverse_pca(self, r: int = -1, add_mean: bool = True) -> ndarray:
         """
         Reconstruct data matrix from the PCA format
         @param  r : int    number of principal components to use. If r <= 0, use all principal components
-        @param  add_mean   if True, add the mean to the reconstructed data. Otherwise, return centered data
+        @param  add_mean   A debug flag. If True, add the mean back to the reconstructed data.
+                           Otherwise, return centered data
         """
         self.validate()
-        u, s, vh = self
+
+        u, s, vh, mean = self
 
         N, n = u.shape[0], vh.shape[1]
 
         # if n<0, it means we want to reconstruct the original data
         if r <= 0:
             r = s.shape[0]
+        elif r > s.shape[0]:
+            raise ValueError(f"r={r} is too large. There are only {s.shape[0]} principal components")
 
         # Get the singular value matrix
         sr = np.zeros((N, n))
@@ -79,7 +92,7 @@ class PCA:
         ret_val = u @ sr @ vh
 
         if add_mean:
-            ret_val += self._mean
+            ret_val += mean
 
         if self._transpose:
             ret_val = ret_val.T
@@ -93,12 +106,15 @@ class PCA:
         return self.__str__()
 
     @staticmethod
-    def pca(data_matrix: np.ndarray, treat_columns_as_datapoints: bool = False) -> PCA:
+    def pca(data_matrix: ndarray, treat_columns_as_datapoints: bool = False) -> PCA:
         """
         Converts a data matrix to PCA format, i.e. svd on centered data.
         :param   data_matrix : array, shape (N, n). Assume n >= 2 and N >= 2
-        :param   treat_columns_as_datapoints: By default, rows are treated as datapoints. If this is True, then columns are treated as datapoints
-        :return  PCAResult: U, s, Vh such that data_matrix == U @ diag(s) @ Vh,
+        :param   treat_columns_as_datapoints: Treat `data_matrix` as a (n, N) matrix instead of (N, n).
+                 Transposes the data matrix to use columns as datapoints instead of rows.
+                 Set this flag to true if you want to treat columns as datapoints.
+                 The `reverse_pca` method respects this flag.
+        :return  PCA instance with U, s, Vh such that data_matrix == U @ diag(s) @ Vh,
                  U: array (N, N), unitary
                  S: (n,) array of singular values. diag(s) is the (N,m) matrix from literature
                  Vh: array (n, n), unitary
@@ -108,7 +124,7 @@ class PCA:
         if treat_columns_as_datapoints:
             data_matrix = data_matrix.T
 
-        # 2D nontrivial matrix
+        # 2D nontrivial matrix, at least 2x2
         assert len(data_matrix.shape) == 2 and data_matrix.shape[1] >= 2 and data_matrix.shape[0] >= 2
 
         # Average data point, vector \overline{x} \in \mathbb{R}^n
@@ -121,9 +137,5 @@ class PCA:
         # u and v are unitary: v @ vh == 1
         u, s, vh = np.linalg.svd(centered_data, full_matrices=True)
 
-        to_ret = PCA(u, s, vh)
-        to_ret._mean = average_datapoint
-        to_ret._transpose = treat_columns_as_datapoints
+        to_ret = PCA(u, s, vh, average_datapoint, treat_columns_as_datapoints)
         return to_ret
-    
-
