54e2f9f225082f460968d83c015df957dc4e523d
Siva Karthikeya <writetokarthikeya99@gmail.com>
2024-02-08 21:01:52 +0100

    Task 4 - Neural tangent GP implementation remaining

---
+++ FinalProject/src/models/convolutional_neural_networks/cnn.py
@@ -0,0 +1,28 @@
+from tensorflow import keras
+from keras.models import Model
+from keras.layers import Conv1D, BatchNormalization, Dense, Flatten, Dropout, InputLayer, Input
+
+
+
+def get_cnn_model():
+    i_layer = Input(shape=(28, 28, 1))
+    x = Conv1D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu')(i_layer)
+    x = BatchNormalization(momentum=0.9)(x)
+    x = Dropout(0.2)(x)
+
+    x = Conv1D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu')(x)
+    x = BatchNormalization(momentum=0.9)(x)
+    x = Dropout(0.2)(x)
+
+    x = Conv1D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu')(x)
+    x = BatchNormalization(momentum=0.9)(x)
+    x = Dropout(0.2)(x)
+
+    x = Flatten()(x)
+    x = Dense(units=128, activation='relu')(x)
+    x = Dropout(0.2)(x)
+
+    o_layer = Dense(units=10, activation='softmax')(x)
+
+    model = Model(i_layer, o_layer)
+    return model
